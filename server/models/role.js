15a05be0c25c38530077ba3310e1df689e1087e3
andela-jwarugu <joy.warugu@andela.com>
2016-12-01 13:53:43 +0300

    [Chore] Debugging chores

--- server/models/role.js
+++ server/models/role.js
@@ -6,8 +6,6 @@ var roleSchema = new Schema ({
     type: String,
     required: true,
     unique: true,
-    default: 'user',
-    enum: ['admin', 'user']
   },
   permissions: {
     type: String,
@@ -15,32 +13,5 @@ var roleSchema = new Schema ({
     enum: ['read', 'readwrite']
   }
 });
-
-roleSchema.pre('save', function(next) {
-  var permissionsDefaults = {
-    'admin': 'readwrite',
-    'user': 'read'
-  };
-
-  this.permissions = permissionsDefaults[this.title];
-  next();
-});
-
-roleSchema.statics.initialize = function () {
-  const role = this;
-  return new Promise((resolve, reject) => {
-    let defaults = role.schema.paths.title.enumValues;
-    defaults = defaults.map((value) => {
-      return { title: value };
-    });
-
-    role.create(defaults, (err, values) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(values);
-    });
-  });
-};
 
 module.exports = mongoose.model('Role', roleSchema);
