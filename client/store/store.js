e9928f13cb606a7db147cc4339d089ff34e8b79d
andela-jwarugu <joy.warugu@andela.com>
2016-12-01 12:01:02 +0300

    [Chore] Heroku build

--- client/store/store.js
+++ client/store/store.js
@@ -1,9 +1,6 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
-// import createLogger from 'redux-logger';
-
-// const logger = createLogger();
 
 const configureStore = (initialState) => {
   return createStore(
@@ -12,7 +9,6 @@ const configureStore = (initialState) => {
     compose(
       applyMiddleware(
         thunk
-        // logger
       ),
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
     );
